8a2799653cdb1cff6fac68f9448e838165191688
Felipe Nolleto Nascimento <0094078@adm.feevale.br>
2017-11-29 21:27:22 -0200

    Created storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,55 +1,55 @@
-// import { isNil } from 'lodash';
+import { isNil } from 'lodash';
 
-// const getOrStoreMaker = <TKey, TValue>(storage: Map<TKey, TValue>) => (key: TKey, getValue: () => TValue) => {
-//   const cached = storage.get(key);
-//   if (!isNil(cached)) return cached;
+const getOrStoreMaker = <TKey, TValue>(storage) => (key: TKey, getValue: () => TValue) => {
+  const cached = storage.get(key);
+  if (!isNil(cached)) return cached;
 
-//   const result = getValue();
-//   storage.set(key, result);
-//   return result;
-// }
+  const result = getValue();
+  storage.set(key, result);
+  return result;
+}
 
-// const isStoredMaker = <TKey, TValue>(storage: Map<TKey, TValue>) => (key: TKey) => {
-//   const cached = storage.get(key);
+const isStoredMaker = <TKey>(storage) => (key: TKey) => {
+  const cached = storage.get(key);
   
-//   return !isNil(cached);
-// }
+  return !isNil(cached);
+}
 
-// const storeMaker = <TKey, TValue>(storage: Map<TKey, TValue>) => (key: TKey, value: TValue) => {
-//   storage.set(key, value);
-// }
+const storeMaker = <TKey, TValue>(storage) => (key: TKey, value: TValue) => {
+  storage.set(key, value);
+}
 
-// const getStoredMaker = <TKey, TValue>(storage: Map<TKey, TValue>) => (key: TKey) => {
-//   return storage.get(key);
-// }
+const getStoredMaker = <TKey>(storage) => (key: TKey) => {
+  return storage.get(key);
+}
 
-// const removeStoredMaker = <TKey, TValue>(storage: Map<TKey, TValue>) => (key: TKey) => {
-//   storage.delete(key);
-// }
+const removeStoredMaker = <TKey>(storage) => (key: TKey) => {
+  storage.delete(key);
+}
 
-// const createDefaultStorage = <TKey, TValue>(storage: Map<TKey, TValue> | WeakMap<object, TValue>) => {
-//   const weak = new WeakMap();
-//   const map = new Map();
-//   const getOrStore = getOrStoreMaker(storage);
-//   const isStored = isStoredMaker(storage);
-//   const getStored = getStoredMaker(storage);
-//   const store = storeMaker(storage);
-//   const removeStored = removeStoredMaker(storage);
+const createDefaultStorage = <TKey, TValue>(storage: Map<TKey, TValue> | WeakMap<object, TValue>) => {
+  const weak = new WeakMap();
+  const map = new Map();
+  const getOrStore = getOrStoreMaker(storage);
+  const isStored = isStoredMaker(storage);
+  const getStored = getStoredMaker(storage);
+  const store = storeMaker(storage);
+  const removeStored = removeStoredMaker(storage);
 
-//   return {
-//     getOrStore,
-//     isStored,
-//     getStored,
-//     store,
-//     removeStored,
-//   };
-// };
+  return {
+    getOrStore,
+    isStored,
+    getStored,
+    store,
+    removeStored,
+  };
+};
 
-// export const createWeakSotorage = <TKey extends object, TValue>(storage: WeakMap<TKey, TValue>) => {
-//   return createDefaultStorage(storage);
-// };
+export const createWeakStorage = <TKey extends object, TValue>(storage?: WeakMap<TKey, TValue>) => {
+  return createDefaultStorage(storage || new WeakMap());
+};
 
-// export const createStorage = <TKey, TValue>(storage: Map<TKey, TValue>) => {
-//   return createDefaultStorage(storage);
-// };
+export const createStorage = <TKey, TValue>(storage?: Map<TKey, TValue>) => {
+  return createDefaultStorage(storage || new Map());
+};
 
--- test/utils/storage.ts
+++ test/utils/storage.ts
@@ -1,20 +1,77 @@
 import * as expect from 'expect';
-import {  } from '../../src/utils/index';
+import { createWeakStorage, createStorage } from '../../src/utils/index';
 
+
+const shouldStoreValue = (storage) => {
+  const key = { foo: 'bar'};
+  const value = { name: 'Jonas' };
+
+  expect(storage.isStored(key)).toBeFalsy();
+  storage.store(key, value);
+  expect(storage.isStored(key)).toBeTruthy();
+  expect(storage.getStored(key)).toEqual(value);
+};
+
+const shouldRemoveValue = (storage) => {
+  const key = { foo: 'bar'};
+  const value = { name: 'Jonas' };
+
+  storage.store(key, value);
+  storage.removeStored(key);
+  expect(storage.isStored(key)).toBeFalsy();
+};
+
+const shouldStoreIfValueDoesntExists = (storage) => {
+  const key = { foo: 'bar'};
+  const value1 = { name: 'Jonas' };
+  const valueStored1 = storage.getOrStore(key, () => value1);
+  
+  expect(valueStored1).toBe(value1);
+  expect(storage.getStored(key)).toBe(value1);
+  expect(storage.isStored(key)).toBeTruthy();
+}
+
+const shouldNotStoreIfValueExists = (storage) => {
+  const key = { foo: 'bar'};
+  const value1 = { name: 'Jonas' };
+  const value2 = { name: 'Philip' };
+
+  const valueStored1 = storage.getOrStore(key, () => value1);
+  const valueStored2 = storage.getOrStore(key, () => value2);
+  
+  expect(valueStored1).toBe(value1);
+  expect(valueStored2).toBe(value1);//already stored
+}
+
+const stores = {
+  'Map': createStorage,
+  'Weak Map': createWeakStorage,
+};
+
+const tests = {
+  'should store value': shouldStoreValue,
+  'should remove value': shouldRemoveValue,
+  'should store if value doesnt exists': shouldStoreIfValueDoesntExists,
+  'should not store if value exists': shouldNotStoreIfValueExists,
+}
 
 describe('utils', () => {
   describe('storage', () => {
-    it('should store', () => {
-      // const storage = createStorage(new WeakMap<object, any>());
-      // const key = 'key';
-      // const value = {
-      //   name: 'Jonas',
-      //   age: 22
-      // };
+    const storesNames = Object.keys(stores);
+    const testsNames = Object.keys(tests);
 
-      // storage.store(key, value);
-      // expect(storage.isStored(key)).toBeTruthy();
-      // expect(storage.getStored(key)).toEqual(value);
-    });
+    for (const storeName of storesNames) {
+      const store = stores[storeName];
+
+      for (const testName of testsNames) {
+        const testMethod = tests[testName];
+        const storage = store();
+
+        it(
+          `[${storeName}] ${testName}`,
+          () => testMethod(storage)
+        );
+      }
+    }
   });
 });
