faa1b95476917eae6013d4ad8593e1e74b2a48e8
nolleto <felipenolletonascimento@gmail.com>
2020-03-23 16:44:28 -0300

    feat(infer-all): add `precision` option to round network result values

--- src/utils/inferAll.ts
+++ src/utils/inferAll.ts
@@ -1,5 +1,7 @@
+import * as roundTo from 'round-to'
+
 import { ICombinations, IInferAllOptions, INetwork, INetworkResult, INode, INodeResult } from '../types'
-import { assoc, clone, identity, ifElse, nthArg, pipe, propEq, reduce } from 'ramda'
+import { assoc, clone, identity, ifElse, mergeRight, nthArg, pipe, propEq, reduce } from 'ramda'
 import { getNodeStates, getNodesFromNetwork } from './network'
 
 import { infer } from '../inferences/junctionTree'
@@ -7,21 +9,24 @@ import { propIsNotNil } from './fp'
 
 const defaultOptions: IInferAllOptions = {
   force: false,
+  precision: 8,
 }
 
-const inferNodeState = (network: INetwork, nodeId: string, nodeState: string, given: ICombinations) => {
+const getOptions = mergeRight(defaultOptions)
+
+const inferNodeState = (network: INetwork, nodeId: string, nodeState: string, given: ICombinations, options: IInferAllOptions) => {
   if (propIsNotNil(nodeId, given)) {
     return propEq(nodeId, nodeState, given) ? 1 : 0
   }
 
-  return infer(network, { [nodeId]: nodeState }, given)
+  return roundTo(infer(network, { [nodeId]: nodeState }, given), options.precision!)
 }
 
-const inferNode = (network: INetwork, node: INode, given: ICombinations) =>
+const inferNode = (network: INetwork, node: INode, given: ICombinations, options: IInferAllOptions) =>
   reduce(
     (acc, nodeState) => assoc(
       nodeState,
-      inferNodeState(network, node.id, nodeState, given),
+      inferNodeState(network, node.id, nodeState, given, options),
       acc,
     ),
     {} as INodeResult,
@@ -34,12 +39,13 @@ const cloneIfForce: <T>(network: T, options: IInferAllOptions) => T = ifElse(
   identity,
 )
 
-export const inferAll = (network: INetwork, given: ICombinations = {}, options: IInferAllOptions = defaultOptions): INetworkResult => {
-  const networkToInfer = cloneIfForce(network, options)
-  const givenToInfer = cloneIfForce(given, options)
+export const inferAll = (network: INetwork, given: ICombinations = {}, options: IInferAllOptions = {}): INetworkResult => {
+  const finalOptions = getOptions(options)
+  const networkToInfer = cloneIfForce(network, finalOptions)
+  const givenToInfer = cloneIfForce(given, finalOptions)
 
   return reduce(
-    (acc, node) => assoc(node.id, inferNode(networkToInfer, node, givenToInfer), acc),
+    (acc, node) => assoc(node.id, inferNode(networkToInfer, node, givenToInfer, finalOptions), acc),
     {} as INetworkResult,
     getNodesFromNetwork(network),
   )
